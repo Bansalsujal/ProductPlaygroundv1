765a6a6fc3a08ca024ad2e447b053b1c748c8823
Bansalsujal <114503822+Bansalsujal@users.noreply.github.com>
2025-09-14 21:33:32 +0530

    Delete ProductPlaygroundv2/ProductPlaygroundv2 directory

--- ProductPlaygroundv2/ProductPlaygroundv2/api/index.js
+++
@@ -1,4 +0,0 @@
-// Vercel serverless function handler
-import app from './server.js';
-
-export default app;
--- ProductPlaygroundv2/ProductPlaygroundv2/netlify/functions/server.js
+++
@@ -1,4 +0,0 @@
-import serverless from 'serverless-http';
-import app from '../../api/server.js';
-
-export const handler = serverless(app);
