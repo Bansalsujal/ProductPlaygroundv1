c7912c8c27b289810a45deaed5da1ac389bbbbf5
Bansalsujal <114503822+Bansalsujal@users.noreply.github.com>
2025-09-14 17:36:46 +0530

    Delete ProductPlaygroundv1 (1)/ProductPlaygroundv1 directory

--- ProductPlaygroundv1 (1)/ProductPlaygroundv1/ProductPlaygroundv1/ProductPlayground/src/components/ui/use-toast.js
+++
@@ -1,7 +0,0 @@
-// Simple toast implementation
-let toastQueue = [];
-
-export const toast = ({ title, description, variant = 'default' }) => {
-  console.log(`Toast [${variant}]: ${title}${description ? ' - ' + description : ''}`);
-  // For now, just log to console. In production you'd implement a proper toast system
-};
